9c5f4d91b7300ea7f28df65af7798e61400136fb
Heikki Vesterinen <hvesteri@gmail.com>
2021-08-27 20:20:26 +0300

    PetStore sample updated

--- sample/PetStore/App.fs
+++ sample/PetStore/App.fs
@@ -2,8 +2,9 @@ namespace PetStore
 
 open System
 open System.Threading.Tasks
-open EndpointBuilder
 open FSharp.Control.Tasks
+open Giraffe
+open EndpointBuilder
 
 module App =
 
@@ -16,19 +17,36 @@ module App =
                     and! petRepo = fromServices<PetRepo>
                     return task {
                         match! petRepo.UpdatePet pet with
-                        | None -> return ClientError.notFound "Pet not found"
-                        | Some updatedPet -> return Response.json updatedPet
+                        | false -> return ErrorResponse.NotFound "Pet not found"
+                        | true -> return Response.NoContent()
                     }
-                })
+                }
+                |> withSummary "Update an existing pet")
 
             post ""
                 (handler {
                     let! pet = fromJsonBody<Pet>
                     and! petRepo = fromServices<PetRepo>
                     return task {
-                        let! addedPet = petRepo.AddPet pet
-                        return Response.json addedPet
+                        let! _ = petRepo.AddPet pet
+                        return Response.Created()
                     }
-                })
+                }
+                |> withSummary "Add a new pet to the store")
+
+            subRoutef "/{petId:%i}" (fun petIdFromPath -> [
+
+                get ""
+                    (handler {
+                        let! petId = petIdFromPath
+                        and! petRepo = fromServices<PetRepo>
+                        return task {
+                            match! petRepo.GetPet(petId) with
+                            | None -> return ErrorResponse.NotFound "Pet not found"
+                            | Some pet -> return Response.Ok pet
+                        }
+                    }
+                    |> withSummary "Find pet by ID")
+            ])
         ]
     ]
--- sample/PetStore/PetRepo.fs
+++ sample/PetStore/PetRepo.fs
@@ -9,6 +9,11 @@ type PetRepo() =
     let mutable nextIndex = 0
     let pets = Dictionary<int, Pet>()
 
+    member _.GetPet(petId) =
+        match pets.TryGetValue petId with
+        | false, _ -> Task.FromResult(None)
+        | true, pet -> Task.FromResult(Some pet)
+
     member _.AddPet(pet : Pet) =
         let petWithId = { pet with Id = nextIndex }
         pets.Add(nextIndex, petWithId)
@@ -18,7 +23,7 @@ type PetRepo() =
     member _.UpdatePet(pet : Pet) =
         if pets.ContainsKey pet.Id then
             pets.[pet.Id] <- pet
-            Task.FromResult(Some pet)
+            Task.FromResult(true)
         else
-            Task.FromResult(None)
+            Task.FromResult(false)
 
